3cd682e975df85a5f7d9d36cc1f60ed7260e7863
Shawn Zhu <shawn@tumblr.com>
2015-08-23 16:27:48 -0700

    Tip Calculator

---
+++ Tipper/SettingsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsViewController.swift
+//  Tipper
+//
+//  Created by Shawn Zhu on 8/22/15.
+//  Copyright (c) 2015 Shawn Zhu. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    let DEFAULT_TIPS_INDEX = "tipsSegmentedControlIndex"
+    @IBOutlet weak var tipsSegmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var index = defaults.integerForKey(DEFAULT_TIPS_INDEX)
+        tipsSegmentedControl.selectedSegmentIndex = index
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    */
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // refresh the tip page with the new default percentage
+        var index = tipsSegmentedControl.selectedSegmentIndex;
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(index, forKey: DEFAULT_TIPS_INDEX)
+        defaults.synchronize()
+        println("set defaults index \(index)")
+    }
+
+
+}
---
+++ Tipper/TipViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Shawn Zhu on 8/21/15.
+//  Copyright (c) 2015 Shawn Zhu. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    let DEFAULT_TIPS_INDEX = "tipsSegmentedControlIndex"
+    let KEY_BILL_AMOUNT = "billAmount"
+    let KEY_LAST_VISIT = "lastVisit"
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var index = defaults.integerForKey(DEFAULT_TIPS_INDEX)
+        var billText = defaults.objectForKey(KEY_BILL_AMOUNT) as? String
+
+        tipControl.selectedSegmentIndex = index
+        
+        if billText != nil {
+            billField.text = billText
+            onEditingChanged(billField)
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercents = [0.18, 0.2, 0.22]
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tipPercent = tipPercents[tipControl.selectedSegmentIndex]
+        var tip = billAmount * tipPercent
+        var total = billAmount + tip
+        var formatter = NSNumberFormatter();
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(NSDate(), forKey: KEY_LAST_VISIT)
+
+        println(sender)
+        if sender as! NSObject == billField {
+            defaults.setObject(billField.text, forKey: KEY_BILL_AMOUNT)
+        }
+
+        formatter.numberStyle = .CurrencyStyle
+        tipLabel.text = formatter.stringFromNumber(tip)
+        totalLabel.text = formatter.stringFromNumber(total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
